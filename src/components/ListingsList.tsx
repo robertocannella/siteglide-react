6f6cd81062d89bfa5d0c4dc950965d03ab919426
robertocannella <robertocannella@gmail.com>
2023-05-13 10:57:07 -0400

    Extract query logic

--- src/components/ListingsList.tsx
+++ src/components/ListingsList.tsx
@@ -1,37 +1,27 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import Listing from '../models/Listing';
-import { useQuery } from '@tanstack/react-query';
 import LoadingSpinner from './Spinner/LoadingSpinner';
+import useListings from '../hooks/useListings';
 
 
-// Using react-query gives auto retries, auto refresh, caching, 
+
 
 const ListingsList = () => {
-  const fetchListings = () => 
-    axios 
-        .get('https://nuvolagraph.staging.oregon.platform-os.com/api/all-listings.json')
-        .then (res => res.data.models.results as Listing[])
-    
-  const {data: listings, error, isLoading} = useQuery<Listing[],Error>({
-    queryKey: ['listings'],  // can be objects as well
-    queryFn: fetchListings
-  })
 
+    const {data: listings, error, isLoading} = useListings();
 
-if (error) return <p>{error.message}</p>;
+    if (error) return <p>{error.message}</p>;
 
+    if (isLoading) return <><LoadingSpinner/></>
 
-if (isLoading) return <><LoadingSpinner/></>
-  return (
-    <ul className="list-group">
-      {listings?.map((listing: Listing) => (
-        <li key={listing.id} className="list-group-item">
-          {listing.properties.address}
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="list-group">
+        {listings?.map((listing: Listing) => (
+            <li key={listing.id} className="list-group-item">
+            {listing.properties.address}
+            </li>
+        ))}
+        </ul>
+    );
 };
 
 export default ListingsList;
---
+++ src/hooks/useListings.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import Listing from '../models/Listing';
+import { useQuery } from '@tanstack/react-query';
+
+
+
+const useListings = () => {
+    const fetchListings = () => 
+    axios 
+        .get('https://nuvolagraph.staging.oregon.platform-os.com/api/all-listings.json')
+        .then (res => res.data.models.results as Listing[])
+
+    // Using react-query gives auto retries, auto refresh, caching, 
+    return useQuery<Listing[],Error>({
+        queryKey: ['listings'],  // can be objects as well
+        queryFn: fetchListings
+      })
+}
+export default useListings
